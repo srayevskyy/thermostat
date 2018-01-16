489c4069b405b43151d76ebd00c5be96f940799f
srayevskyy <you@example.com>
2018-01-16 02:14:05 -0800

    Added network interface restart

--- golang_gpio_thermostat_control/gpio_server_runner.sh
+++ golang_gpio_thermostat_control/gpio_server_runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S %p'
+LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S'
 
 if ps axu | grep --silent "[g]pio_server.go"; then
   echo "`date \"${LOGDATEFORMAT}\"` INFO [${0}] GPIO server is already running, exiting"
@@ -13,4 +13,4 @@ killall -q gpio_server
 
 export PATH=$PATH:/usr/local/go/bin
 
-nohup go run /home/pi/src/thermostat/golang_gpio_thermostat_control/gpio_server.go >& gpio_server.log &
+nohup go run /home/pi/src/thermostat/golang_gpio_thermostat_control/gpio_server.go >> gpio_server.log 2>&1 &
--- pizero-camera-downloader/bin/pizero-camera-downloader-runner.sh
+++ pizero-camera-downloader/bin/pizero-camera-downloader-runner.sh
@@ -2,6 +2,13 @@
 
 LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S'
 
+camera_ip=192.168.1.254
+
+if ! ping -Iwlan1 -c1 ${camera_ip}; then
+  echo "`date \"${LOGDATEFORMAT}\"` [${0}] Warning: cannot reach camera by ip ${camera_ip}, trying to restart network interface"
+  sudo ifconfig wlan1 down && sudo ifconfig wlan1 up && sleep 10
+fi
+
 if ps axu | grep --silent "[p]izero-camera-downloader.go"; then
   echo "`date \"${LOGDATEFORMAT}\"` INFO [${0}] downloader already running, exiting"
   exit 0
