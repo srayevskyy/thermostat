976566daa582fdc3a999f66bd7cbc013e0f2b657
Your Name <you@example.com>
2018-03-25 16:42:00 -0700

    Added ip addres to display

--- python_air_quality_monitor/ccs811.py
+++ python_air_quality_monitor/ccs811.py
@@ -4,11 +4,28 @@ from Adafruit_CCS811 import Adafruit_CCS811
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
+import datetime
+import socket
+import fcntl
+import struct
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 from time import sleep
-import datetime
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+co2Value = 0
+tvocValue = 0
+tempValue = 0
 
 ccs = Adafruit_CCS811()
 
@@ -17,10 +34,6 @@ while not ccs.available():
 
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
-
-co2Value = 0
-tvocValue = 0
-tempValue = 0
 
 if ccs.available():
     while (co2Value == 0):
@@ -37,9 +50,12 @@ disp.display()
 image = Image.new('1', (disp.width, disp.height))
 font = ImageFont.truetype('../fonts/red_alert.ttf', 14)
 draw = ImageDraw.Draw(image)
-draw.text((0, 0), "CO2 : {}".format(co2Value), font=font, fill=255)
-draw.text((0, 16), "TVOC: {}".format(tvocValue), font=font, fill=255)
-draw.text((0, 32), "Temp: {} C".format(tempValue), font=font, fill=255)
-draw.text((0, 48), "Last: {}".format(datetime.datetime.today().strftime("%b, %d %H:%M")), font=font, fill=255)
+draw.text((0, 0), "CO2: {} TVOC: {}".format(
+    co2Value, tvocValue), font=font, fill=255)
+draw.text((0, 16), "Temp: {} C".format(tempValue), font=font, fill=255)
+draw.text((0, 32), "Last: {}".format(
+    datetime.datetime.today().strftime("%b, %d %H:%M:%S")), font=font, fill=255)
+draw.text((0, 48), "IP: {}".format(
+    get_ip_address('wlan0')), font=font, fill=255)
 disp.image(image)
 disp.display()
