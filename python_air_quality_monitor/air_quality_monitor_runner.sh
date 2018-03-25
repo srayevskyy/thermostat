1a00f7c7b86e001a35b7d7c0649f2a63cb0a244b
Your Name <you@example.com>
2018-03-25 04:20:56 -0700

    Added runer for air quality monitor

---
+++ python_air_quality_monitor/air_quality_monitor_runner.sh
@@ -0,0 +1,12 @@
+LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S %p'
+
+if ps axu | grep --silent "[c]cs811.py"; then
+  echo "`date \"${LOGDATEFORMAT}\"` INFO [${0}] Air quality monitor program (ccs811.py) is already running, exiting"
+  exit 0
+fi
+
+cd /home/pi/go/src/github.com/srayevskyy/thermostat/python_air_quality_monitor
+
+sudo python ccs811.py
+
+
