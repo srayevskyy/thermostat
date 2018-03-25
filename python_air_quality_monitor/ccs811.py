f9fc7e08fb599db600408d1ae524f52e434295ec
Your Name <you@example.com>
2018-03-25 04:16:05 -0700

    Now displaying info on OLED

--- python_air_quality_monitor/ccs811.py
+++ python_air_quality_monitor/ccs811.py
@@ -1,10 +1,14 @@
 # this example reads and prints CO2 equiv. measurement, TVOC measurement, and temp every 2 seconds
 
-from time import sleep
 from Adafruit_CCS811 import Adafruit_CCS811
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
-# initialize fonts for render on the screen
-#font = ImageFont.load_default()
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from time import sleep
+import datetime
 
 ccs = Adafruit_CCS811()
 
@@ -26,4 +30,16 @@ if ccs.available():
             tvocValue = ccs.getTVOC()
         sleep(1)
 
-print "CO2: ", co2Value, "TVOC: ", tvocValue, "temp: ", tempValue
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=24, i2c_bus=0)
+disp.begin()
+disp.clear()
+disp.display()
+image = Image.new('1', (disp.width, disp.height))
+font = ImageFont.truetype('../fonts/red_alert.ttf', 14)
+draw = ImageDraw.Draw(image)
+draw.text((0, 0), "CO2 : {}".format(co2Value), font=font, fill=255)
+draw.text((0, 16), "TVOC: {}".format(tvocValue), font=font, fill=255)
+draw.text((0, 32), "Temp: {} C".format(tempValue), font=font, fill=255)
+draw.text((0, 48), "Last: {}".format(datetime.datetime.today().strftime("%b, %d %H:%M")), font=font, fill=255)
+disp.image(image)
+disp.display()
