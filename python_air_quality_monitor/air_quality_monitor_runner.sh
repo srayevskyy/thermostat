5c7472190811993112dfb8bf3f848939ec4e5acd
Your Name <you@example.com>
2018-03-25 23:07:47 -0700

    Parameters improvements

--- python_air_quality_monitor/air_quality_monitor_runner.sh
+++ python_air_quality_monitor/air_quality_monitor_runner.sh
@@ -1,11 +1,11 @@
 LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S %p'
 
-if ps axu | grep --silent "[c]cs811.py"; then
+if ps axu | grep --silent "[c]cs811_webserver.py"; then
   echo "`date \"${LOGDATEFORMAT}\"` INFO [${0}] Air quality monitor program (ccs811.py) is already running, exiting"
   exit 0
 fi
 
 cd /home/pi/go/src/github.com/srayevskyy/thermostat/python_air_quality_monitor
 source ./venv/bin/activate
-python ./ccs811.py
+python ./ccs811_webserver.py
 
--- python_air_quality_monitor/ccs811_webserver.py
+++ python_air_quality_monitor/ccs811_webserver.py
@@ -3,12 +3,14 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
 from flask_apscheduler import APScheduler
-
 from Adafruit_CCS811 import Adafruit_CCS811
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 import time
 import datetime
+import socket
+import fcntl
+import struct
 
 from PIL import Image
 from PIL import ImageFont
@@ -17,6 +19,8 @@ from PIL import ImageDraw
 TIMEFORMAT = '%b, %d %H:%M:%S'
 BIND_ADDR = '0.0.0.0'
 BIND_PORT = 5002
+I2C_BUS_OLED = 0
+I2C_BUS_CCS811 = 1
 
 co2Value = 0
 tvocValue = 0
@@ -24,10 +28,39 @@ tempValue = 0
 lastTimeSensorRead = datetime.datetime.today()
 
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+
+def display_sensor_values():
+    global co2Value, tvocValue, tempValue, lastTimeSensorRead
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=24, i2c_bus=I2C_BUS_OLED)
+    disp.begin()
+    disp.clear()
+    disp.display()
+    image = Image.new('1', (disp.width, disp.height))
+    font = ImageFont.truetype('../fonts/red_alert.ttf', 14)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), "CO2: {} TVOC: {}".format(
+        co2Value, tvocValue), font=font, fill=255)
+    draw.text((0, 16), "Temp: {} C".format(tempValue), font=font, fill=255)
+    draw.text((0, 32), "Last: {}".format(
+        lastTimeSensorRead.strftime(TIMEFORMAT)), font=font, fill=255)
+    draw.text((0, 48), "IP: {}".format(
+        get_ip_address('wlan0')), font=font, fill=255)
+    disp.image(image)
+    disp.display()
+
+
 def read_sensor_values():
     global co2Value, tvocValue, tempValue, lastTimeSensorRead
 
-    ccs = Adafruit_CCS811()
+    ccs = Adafruit_CCS811(busnum=I2C_BUS_CCS811)
 
     while not ccs.available():
         pass
@@ -52,8 +85,14 @@ class Config(object):
             'id': 'read_sensor_values',
             'func': read_sensor_values,
             'trigger': 'interval',
+            'seconds': 60
+        },
+        {
+            'id': 'display_sensor_values',
+            'func': display_sensor_values,
+            'trigger': 'interval',
             'seconds': 20
-        }
+        },
     ]
 
     SCHEDULER_API_ENABLED = True
@@ -89,8 +128,9 @@ if __name__ == '__main__':
     api.add_resource(SensorValueByType, '/sensor_value_by_type/<valueType>')
     app.config.from_object(Config())
 
-    # initial read of sensor values
+    # initial read and display of sensor values
     read_sensor_values()
+    display_sensor_values()
 
     scheduler = APScheduler()
     scheduler.init_app(app)
