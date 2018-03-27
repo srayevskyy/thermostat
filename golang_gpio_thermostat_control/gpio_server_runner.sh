7d93d9778d94118182f08cb7197a0e02188c05e8
Your Name <you@example.com>
2018-03-27 01:25:30 -0700

    Modified runner, added glide.yaml

--- golang_gpio_thermostat_control/gpio_server_runner.sh
+++ golang_gpio_thermostat_control/gpio_server_runner.sh
@@ -7,10 +7,10 @@ if ps axu | grep --silent "[g]pio_server.go"; then
   exit 0
 fi
 
-cd /home/pi/src/thermostat/golang_gpio_thermostat_control
+cd /home/pi/go/src/github.com/srayevskyy/thermostat/golang_gpio_thermostat_control
 
 killall -q gpio_server
 
 export PATH=$PATH:/usr/local/go/bin
 
-nohup go run /home/pi/src/thermostat/golang_gpio_thermostat_control/gpio_server.go >> gpio_server.log 2>&1 &
+nohup go run ./gpio_server.go >> gpio_server.log 2>&1 &
